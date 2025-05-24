2d416ae156c672cdb44ba3f67c03014932523de9
navanken <afandinaufal67@gmail.com>
2025-05-24 21:57:52 +0700

    Delete memo

--- Vite_Prima_React/src/pages/GuruRegisterPage.jsx
+++ Vite_Prima_React/src/pages/GuruRegisterPage.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable no-unused-vars */
-import { useState, useEffect, memo, useCallback } from "react";
+import { useState, useEffect, useCallback } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { motion } from "framer-motion";
 import { registerGuru } from "../api/odoo";
